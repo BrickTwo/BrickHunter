5a7d37670cc5ea1548138f21168cecd19de83d39
Riskun <tobias.karth@gmail.com>
2020-10-15 21:04:04 +0200

    fix bricksAndPieces foward

--- src/background.js
+++ src/background.js
@@ -15,9 +15,15 @@ browser.runtime.onMessage.addListener(function(
     sendResponse,
     reject
 ) {
+    console.log(
+        'country',
+        localStorage.getItem('country'),
+        localStorage.getItem('language')
+    );
     var localeCountry = localStorage.getItem('country') || 'DE';
     var localeLanguage = localStorage.getItem('language') || 'DE';
-    var localeCountryLanguage = localeLanguage.toLowerCase() + '-' + localeCountry.toUpperCase();
+    var localeCountryLanguage =
+        localeLanguage.toLowerCase() + '-' + localeCountry.toUpperCase();
 
     const timeout = setTimeout(function() {}, 5000);
 
@@ -216,7 +222,7 @@ browser.runtime.onMessage.addListener(function(
                 .then((tabs) => {
                     var tab = tabs[0];
                     browser.tabs.update(tab.id, {
-                        url: `https://www.lego.com/${localeCountryLanguage}/service/replacementparts/sale`,
+                        url: `https://www.lego.com/${localeCountryLanguage.toLowerCase()}/service/replacementparts/sale`,
                     });
                 });
             sendResponse(true);
@@ -234,7 +240,7 @@ browser.runtime.onMessage.addListener(function(
                 .then((tabs) => {
                     var tab = tabs[0];
                     browser.tabs.update(tab.id, {
-                        url: `https://www.lego.com/${localeCountryLanguage}/service/replacementparts/sale`,
+                        url: `https://www.lego.com/${localeCountryLanguage.toLowerCase()}/service/replacementparts/sale`,
                     });
                 });
             sendResponse(true);
