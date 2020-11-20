c70c48b5b4914c31b12ae99f36f7de4a65c40611
Riskun <tobias.karth@gmail.com>
2020-11-20 23:58:58 +0100

    Add StoneWars Affiliate

--- src/mixins/affiliateMixin.js
+++ src/mixins/affiliateMixin.js
@@ -92,19 +92,96 @@ export const affiliateMixin = {
                         },
                     ]
                 },
-                /*{
+                {
                     partner: "StoneWars",
                     order: 2,
                     countries: [
                         {
+                            country: "at",
+                            linkType: "webgains",
+                            wgcampaignid: 1351375,
+                            wgprogramid: 268005,
+                            clickref: "BrickHunter"
+                        },
+                        {
+                            country: "be",
+                            linkType: "webgains",
+                            wgcampaignid: 1351375,
+                            wgprogramid: 274945,
+                            clickref: "BrickHunter"
+                        },
+                        {
+                            country: "ch",
+                            linkType: "webgains",
+                            wgcampaignid: 1351375,
+                            wgprogramid: 268015,
+                            clickref: "BrickHunter"
+                        },
+                        {
                             country: "de",
                             linkType: "webgains",
-                            wgcampaignid: 99999,
-                            wgprogramid: 88888,
+                            wgcampaignid: 1351375,
+                            wgprogramid: 267995,
+                            clickref: "BrickHunter"
+                        },
+                        {
+                            country: "dk",
+                            linkType: "webgains",
+                            wgcampaignid: 1351375,
+                            wgprogramid: 274925,
+                            clickref: "BrickHunter"
+                        },
+                        {
+                            country: "es",
+                            linkType: "webgains",
+                            wgcampaignid: 1351375,
+                            wgprogramid: 274875,
+                            clickref: "BrickHunter"
+                        },
+                        {
+                            country: "fi",
+                            linkType: "webgains",
+                            wgcampaignid: 1351375,
+                            wgprogramid: 274915,
+                            clickref: "BrickHunter"
+                        },
+                        {
+                            country: "fr",
+                            linkType: "webgains",
+                            wgcampaignid: 1351375,
+                            wgprogramid: 268085,
+                            clickref: "BrickHunter"
+                        },
+                        {
+                            country: "it",
+                            linkType: "webgains",
+                            wgcampaignid: 1351375,
+                            wgprogramid: 274895,
+                            clickref: "BrickHunter"
+                        },
+                        {
+                            country: "nl",
+                            linkType: "webgains",
+                            wgcampaignid: 1351375,
+                            wgprogramid: 270975,
+                            clickref: "BrickHunter"
+                        },
+                        {
+                            country: "no",
+                            linkType: "webgains",
+                            wgcampaignid: 1351375,
+                            wgprogramid: 274935,
+                            clickref: "BrickHunter"
+                        },
+                        {
+                            country: "se",
+                            linkType: "webgains",
+                            wgcampaignid: 1351375,
+                            wgprogramid: 274905,
                             clickref: "BrickHunter"
                         },
                     ]
-                }*/
+                },
             ],
         };
     },
@@ -117,7 +194,7 @@ export const affiliateMixin = {
             });
 
             return partners.sort(function(a, b) {
-                return a.order - b.order;
+                return b.order - a.order;
             });
         }
     }
