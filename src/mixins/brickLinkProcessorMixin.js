0766efc053a05507927dc24837d79c797f04f864
Riskun <tobias.karth@gmail.com>
2021-02-10 20:20:38 +0100

    check if altno is number

--- src/mixins/brickLinkProcessorMixin.js
+++ src/mixins/brickLinkProcessorMixin.js
@@ -1,27 +1,27 @@
 export const brickLinkProcessorMixin = {
     methods: {
         findAlternateItemNumbers(item) {
-            return item.brickLink.strAltNo.split(',').map(function(number) {
-                return number.trim();
-            });
+            return item.brickLink.strAltNo
+                .split(',')
+                .map(function(number) {
+                    if (!isNaN(number.trim())) return number.trim();
+                })
+                .filter((f) => f != null);
         },
         findColorCodes(item) {
-            //console.log("FindColorCodes", item)
             if (!item.brickLink.mapPCCs) return null;
 
             var colorCodesArray = item.brickLink.mapPCCs;
 
             if (!colorCodesArray[item.color.brickLinkId]) return null;
-            //console.log(colorCodesArray, item.color.brickLinkId)
             var colorCodes = colorCodesArray[item.color.brickLinkId].split(',');
-            //console.log("FindColorCodes", colorCodes)
             return colorCodes;
         },
         returnModelsObject(html) {
             var startPos = html.data.indexOf('blapp.models.set(') + 18;
             var length = html.data.substr(startPos).indexOf(');');
             var models = html.data.substr(startPos, length);
-            //console.log(JSON.parse(models))
+            
             var model = JSON.parse(models).filter(
                 (model) => model.name == 'itemInfo'
             )[0];
@@ -36,17 +36,14 @@ export const brickLinkProcessorMixin = {
             if (item.source != 'brickLink') return item;
 
             if (this.isSpecialBrick(item)) {
-                //console.log("special brick")
                 var desingIds = await this.findColorCodes(item);
                 item.searchids = desingIds;
             } else {
-                //console.log("normal brick")
                 item.searchids = [this.cleanDesignId(item.designId)];
                 item.searchids = item.searchids.concat(
                     await this.findAlternateItemNumbers(item)
                 );
             }
-            //console.log("searchIds", item.searchids)
             return item;
         },
     },
