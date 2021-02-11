6215d118dc7f9f5d7b41822f64fb44779b5cffbc
Riskun <tobias.karth@gmail.com>
2021-02-11 19:00:34 +0100

    error handling webr equests

--- src/mixins/requestsMixin.js
+++ src/mixins/requestsMixin.js
@@ -2,53 +2,84 @@ const axios = require('axios');
 export const requestsMixin = {
     methods: {
         getBricklink(item) {
-            return axios.get(
-                `https://www.bricklink.com/r3/catalog/parts/Brick_Round/Cone/product.page?P=${item}`
-            );
+            var response = null;
+            try {
+                response = axios.get(
+                    `https://www.bricklink.com/r3/catalog/parts/Brick_Round/Cone/product.page?P=${item}`
+                );
+            } catch (err) {}
+            return response;
         },
         sendPrices(items) {
             if (!items) return;
 
-            return axios.post(
-                'https://brickhunter.bricktwo.net/api/bricksandpieces/update.php',
-                items
-            );
+            var response = null;
+            try {
+                response = axios.post(
+                    'https://brickhunter.bricktwo.net/api/bricksandpieces/update.php',
+                    items
+                );
+            } catch (err) {}
+            return response;
         },
         async getCategoriesAsync() {
-            var response = await axios.get(
-                `https://brickhunter.bricktwo.net/api/categories/read.php?country=${this.$store.state.country}`
-            );
+            var response = null;
+            try {
+                response = await axios.get(
+                    `https://brickhunter.bricktwo.net/api/categories/read.php?country=${this.$store.state.country}`
+                );
+            } catch (err) {}
             return response.data;
         },
-        async getBricksAsync($page, $limit, categoryId, colorId, keyword, sortField, sortDirection) {
-            if(!keyword){
+        async getBricksAsync(
+            $page,
+            $limit,
+            categoryId,
+            colorId,
+            keyword,
+            sortField,
+            sortDirection
+        ) {
+            if (!keyword) {
                 keyword = '';
             }
-            if(categoryId == 9999999){
+            if (categoryId == 9999999) {
                 categoryId = '';
             }
 
-            var response = await axios.get(
-                `https://brickhunter.bricktwo.net/api/bricks/read.php?page=${$page}&limit=${$limit}&country=${this.$store.state.country}&category=${categoryId}&color=${colorId}&keyword=${keyword}&sortfield=${sortField}&sortdir=${sortDirection}`
-            );
+            var response = null;
+            try {
+                response = await axios.get(
+                    `https://brickhunter.bricktwo.net/api/bricks/read.php?page=${$page}&limit=${$limit}&country=${this.$store.state.country}&category=${categoryId}&color=${colorId}&keyword=${keyword}&sortfield=${sortField}&sortdir=${sortDirection}`
+                );
+            } catch (err) {}
             return response.data;
         },
         async getBrickAsync($itemNumber) {
-            var response = await axios.get(
-                `https://brickhunter.bricktwo.net/api/bricks/single/read.php?itemnumber=${$itemNumber}&country=${this.$store.state.country}`
-            );
-            return response.data;
+            var response = null;
+            try {
+                response = await axios.get(
+                    `https://brickhunter.bricktwo.net/api/bricks/single/read.php?itemnumber=${$itemNumber}&country=${this.$store.state.country}`
+                );
+            } catch (err) {}
+            return response?.data;
         },
         async getColorsAsync() {
-            var response = await axios.get(
-                `https://brickhunter.bricktwo.net/api/colors/read.php`
-            );
+            var response = null;
+            try {
+                response = await axios.get(
+                    `https://brickhunter.bricktwo.net/api/colors/read.php`
+                );
+            } catch (err) {}
             return response.data;
         },
         async getSyncAsync() {
-            var response = await axios.get(
-                `https://brickhunter.bricktwo.net/api/sync/read.php`
-            );
+            var response = null;
+            try {
+                response = await axios.get(
+                    `https://brickhunter.bricktwo.net/api/sync/read.php`
+                );
+            } catch (err) {}
             return response.data;
         },
     },
