d4604592322cab4d82d932dec0a4159b27570102
Tobias Karth <tobias.karth@adesso.ch>
2023-03-23 10:25:11 +0100

    add color component

---
+++ src/app/shared/components/color/color.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ColorService } from 'src/app/core/services/color.service';
+import { IColor } from 'src/app/models/parts-list';
+
+@Component({
+  selector: 'app-color',
+  templateUrl: './color.component.html',
+  styleUrls: ['./color.component.scss'],
+})
+export class ColorComponent implements OnInit {
+  @Input() colorId: number;
+
+  color: IColor;
+
+  constructor(private readonly colorService: ColorService) {}
+
+  ngOnInit(): void {
+    this.color = this.colorService.getColor(this.colorId, 'Rebrickable');
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { PrimengModule } from '../primeng.module';
+import { ColorComponent } from './components/color/color.component';
 import { ProgressIconComponent } from './components/progress-icon/progress-icon.component';
 import { SideNavigationComponent } from './layout';
 
 @NgModule({
-  declarations: [SideNavigationComponent, ProgressIconComponent],
+  declarations: [SideNavigationComponent, ProgressIconComponent, ColorComponent],
   imports: [PrimengModule, CommonModule, FontAwesomeModule],
-  exports: [SideNavigationComponent, ProgressIconComponent],
+  exports: [SideNavigationComponent, ProgressIconComponent, ColorComponent],
 })
 export class SharedModule {}
