eeaf25b7c3acb7591b2c16718152c3d640eed1ed
Tobias Karth <tobias.karth@gmail.com>
2023-04-08 11:02:55 +0200

    add BH logo and placeholder

--- src/app/browse-parts/components/browse-parts-grid-item/browse-parts-grid-item.component.ts
+++ src/app/browse-parts/components/browse-parts-grid-item/browse-parts-grid-item.component.ts
@@ -54,7 +54,7 @@ export class BrowsePartsGridItemComponent implements OnInit, AfterViewInit, Afte
     return `background-image: url('${
       !!this.part.imageUrl
         ? `https://brickhunter.blob.core.windows.net${this.part.imageUrl}`
-        : './assets/icons/icon-trans48.png'
+        : './assets/placeholder.png'
     }');
             width: 100%;
             height: 120px;
