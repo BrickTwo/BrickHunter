ba51d41a53ecbcc5abffea1a6986edb70bb95b4a
Tobias Karth <tobias.karth@adesso.ch>
2023-04-21 11:32:29 +0200

    Move PDF to export function

--- src/app/parts-list/components/parts-list-export/parts-list-export.component.ts
+++ src/app/parts-list/components/parts-list-export/parts-list-export.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { PartsListService } from '../../services/parts-list.service';
 import { ColorService } from 'src/app/core/services/color.service';
-import { PartsList } from 'src/app/models/parts-list';
+import { Part, PartsList } from 'src/app/models/parts-list';
 import * as xml2js from 'xml2js';
 import { BrickHunterV2, BrickHunterV2Item } from 'src/app/models/brickhunter';
+import autoTable, { ColumnInput } from 'jspdf-autotable';
+import jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-parts-list-export',
@@ -25,11 +27,12 @@ export class PartsListExportComponent {
   selectedFilterValue: string = 'all';
 
   exportToOptions: any[] = [
+    { name: 'PDF', value: 'pdf' },
     { name: 'BrickHunter', value: 'brickHunter' },
     { name: 'BrickLink', value: 'brickLink' },
     { name: 'CSV', value: 'csv' },
   ];
-  selectedExportToValue: string = 'brickHunter';
+  selectedExportToValue: string = 'pdf';
 
   constructor(private readonly partsListService: PartsListService, private readonly colorService: ColorService) {}
 
@@ -40,6 +43,9 @@ export class PartsListExportComponent {
 
   async onExport() {
     switch (this.selectedExportToValue) {
+      case 'pdf':
+        this.exportPDF();
+        break;
       case 'brickHunter':
         this.exportBrickHunter();
         break;
@@ -54,6 +60,98 @@ export class PartsListExportComponent {
 
   onClose() {
     this.display = false;
+  }
+
+  async exportPDF() {
+    const columns: ColumnInput[] = [
+      { header: 'DesignId', dataKey: 'designId' },
+      { header: 'ElementId', dataKey: 'elementId' },
+      { header: 'Image', dataKey: 'image' },
+      { header: 'Color', dataKey: 'color' },
+      { header: 'Quantity', dataKey: 'quantity' },
+      { header: 'BrickLink', dataKey: 'bricklink' },
+      { header: 'PaB', dataKey: 'pab' },
+    ];
+
+    const rows = this.partsListService.getParts(this.partsList.uuid, this.selectedFilterValue).map(async part => {
+      const color = await this.colorService.getColor(part.color);
+      const imgUrl = await this.caclImageUrl(part)
+        .then(imgUrl => imgUrl)
+        .catch(e => './assets/placeholder.png');
+
+      return {
+        designId: part.designId,
+        elementId: part.elementId,
+        color: color?.name,
+        quantity: part.qty,
+        bricklink: part.maxPrice,
+        pab: part.lego ? `${part.lego?.price.amount.toFixed(2)} ${part.lego?.price.currencyCode}` : '',
+        imageUrl: imgUrl,
+      };
+    });
+
+    var totalPagesExp = '{total_pages_count_string}';
+
+    const partsListTitle = `${this.partsList?.name} - ${
+      this.filterOptions.find(o => o.value === this.selectedFilterValue).name
+    }`;
+
+    const doc = new jsPDF('p', 'px', 'a4');
+    autoTable(doc, {
+      columns: columns,
+      body: await Promise.all(rows),
+      startY: 25,
+      bodyStyles: { minCellHeight: 35 },
+      rowPageBreak: 'avoid',
+      didDrawCell: function (data) {
+        if (data.column.dataKey === 'image' && data.cell.section === 'body') {
+          const img = data.row.raw['imageUrl'];
+          const dim = data.cell.height - data.cell.padding('vertical');
+          const textPos = data.cell.getTextPos();
+          doc.addImage(img, textPos.x, textPos.y, dim, dim);
+        }
+      },
+      didDrawPage: function (data) {
+        // Header
+        doc.setFontSize(20);
+        doc.setTextColor(40);
+        doc.text(partsListTitle, data.settings.margin.left, 20);
+
+        // Footer
+        var str = 'Page ' + (doc.internal as any).getNumberOfPages();
+        // Total page number plugin only available in jspdf v1.0+
+        if (typeof doc.putTotalPages === 'function') {
+          str = str + ' of ' + totalPagesExp;
+        }
+        doc.setFontSize(10);
+
+        // jsPDF 1.4+ uses getHeight, <1.4 uses .height
+        var pageSize = doc.internal.pageSize;
+        var pageHeight = pageSize.height ? pageSize.height : pageSize.getHeight();
+        doc.text(str, data.settings.margin.left, pageHeight - 10);
+      },
+    });
+
+    if (typeof doc.putTotalPages === 'function') {
+      doc.putTotalPages(totalPagesExp);
+    }
+
+    doc.save(`${this.partsList.name}-${this.filterOptions.find(o => o.value === this.selectedFilterValue).name}.pdf`);
+  }
+
+  caclImageUrl(part: Part) {
+    if (part.source.source === 'BrickLink')
+      return this.imageExists(`https://img.bricklink.com/ItemImage/PN/${part.source.color}/${part.source.id}.png`);
+    return this.imageExists(`https://brickhunter.blob.core.windows.net/parts/pab/${part.elementId}.jpg`);
+  }
+
+  imageExists(url: string) {
+    return new Promise((resolve, reject) => {
+      var img = new Image();
+      img.onload = () => resolve(url);
+      img.onerror = () => reject();
+      img.src = url;
+    });
   }
 
   exportBrickHunter() {
