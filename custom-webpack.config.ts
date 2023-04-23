ae7fc2567b89865cb948f52c4efe85f0223d06ef
Tobias Karth <tobias.karth@gmail.com>
2023-04-23 10:36:30 +0200

    renaming content-script

--- custom-webpack.config.ts
+++ custom-webpack.config.ts
@@ -3,7 +3,7 @@ import type { Configuration } from 'webpack';
 module.exports = {
   entry: {
     background: { import: 'src/background.ts', runtime: false },
-    contentscript: { import: 'src/content-script.ts', runtime: false },
+    legocontentscript: { import: 'src/lego-content-script.ts', runtime: false },
   },
   resolve: {
     //fallback: { "timers": require.resolve('timers-browserify') }
